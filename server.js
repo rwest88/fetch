3c2232ec491101c660ebc3e16c7dc9489bd1352f
Wynn Blevins <wynnblevins@yahoo.com>
2018-05-11 18:22:43 -0400

    Whitespace changes.

--- server.js
+++ server.js
@@ -25,25 +25,20 @@ app.get('/', function(req, res){
     res.send('Welcome to Passport with Sequelize');
 });
 
-
 //Models
 var models = require("./app/models");
-
 
 //Routes
 var authRoute = require('./app/routes/auth.js')(app,passport);
 
-
 //load passport strategies
 require('./app/config/passport/passport.js')(passport,models.user);
 
-
 //Sync Database
 models.sequelize.sync().then(function(){
-console.log('Nice! Database looks fine')
-
+  console.log('Nice! Database looks fine');
 }).catch(function(err){
-console.log(err,"Something went wrong with the Database Update!")
+  console.log(err,"Something went wrong with the Database Update!");
 });
 
 app.listen(5000, function(err){

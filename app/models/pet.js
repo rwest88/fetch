1afa32e8e20dbbb0fe488d5dd0c85015d2a70c25
Wynn Blevins <wynnblevins@yahoo.com>
2018-05-15 12:24:56 -0400

    Removed outdated copy/paste comments in pet.js

--- app/models/pet.js
+++ app/models/pet.js
@@ -16,8 +16,6 @@ module.exports = function(sequelize, DataTypes) {
     });
   
     Pet.associate = function(models) {
-      // We're saying that a Post should belong to an Author
-      // A Post can't be created without an Author due to the foreign key constraint
       Pet.belongsTo(models.User, {
         foreignKey: {
           allowNull: false
@@ -25,7 +23,5 @@ module.exports = function(sequelize, DataTypes) {
       });
     };
   
-    // Add a belongsTo association to Authors here
-    // Example: https://github.com/sequelize/express-example/blob/master/models/task.js
     return Pet;
  };

1a263ba915d3c7514db14719d77527f8d91f84bd
Wynn Blevins <wynnblevins@yahoo.com>
2018-05-15 11:25:58 -0400

    Removed comment from user.js

--- app/models/user.js
+++ app/models/user.js
@@ -20,21 +20,3 @@ module.exports = function(sequelize, DataTypes) {
 
 	return User;
 }
-
-
-// module.exports = function(sequelize, DataTypes) {
-// 	var User = sequelize.define("User", {
-// 	  // Giving the Author model a name of type STRING
-// 	  name: DataTypes.STRING
-// 	});
-  
-// 	User.associate = function(models) {
-// 	  // Associating Author with Posts
-// 	  // When an Author is deleted, also delete any associated Posts
-// 	  User.hasMany(models.Pet, {
-// 		onDelete: "cascade"
-// 	  });
-// 	};
-  
-// 	return User;
-// };

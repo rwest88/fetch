00cf58f5de7d5b8fdf0411a11d5bc6d3edc1ea28
Wynn Blevins <wynnblevins@yahoo.com>
2018-05-15 20:39:00 -0400

    Added engagements table.

---
+++ app/models/engagements.js
@@ -0,0 +1,29 @@
+module.exports = function(sequelize, DataTypes) {
+    var Engagement = sequelize.define("Engagement", {
+      id: {autoIncrement: true, primaryKey: true, type: DataTypes.INTEGER},
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      date: {type: DataTypes.DATE, allowNull: false}
+    }, {
+      timestamps: false
+    });
+    
+    Engagement.associate = function(models) {
+      Engagement.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+      Engagement.belongsTo(models.Pet, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+    return Engagement;
+  };

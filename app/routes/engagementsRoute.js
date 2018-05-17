71506f79d5e0455e74046cfe6225c7934beb6d74
Wynn Blevins <wynnblevins@yahoo.com>
2018-05-17 14:43:15 -0400

    Added ability to do a put (update) request to engagements endpoint.

--- app/routes/engagementsRoute.js
+++ app/routes/engagementsRoute.js
@@ -53,23 +53,29 @@ module.exports = function (app, db, bodyParser, authCheckService) {
                 id: engagementId
             }
         }).then((response) => {
-            res.status(204); // 204 == No Content
+            res.status(204); // 204 == Success with no content
         }).catch((err) => {
             res.status(500).send('Ooopsie whoopsie: ' + err);
         }); 
     });
 
-    // app.put('/api/engagement/:engagementId', 
-    //     authCheckService.isAuthenticated, (req, res) => {
+    app.put('/api/engagement/:engagementId/pet/:PetId/user/:UserId', 
+        authCheckService.isAuthenticated, (putReq, putRes) => {
 
-
-
-    //     var updatedEngagement = {
-    //         date: req.body.date,
-    //         name: req.body.name,
-    //         address: req.body.address,
-    //         UserId: req.params.UserId,
-    //         PetId: req.params.PetId
-    //     };    
-    // });
+        var updatedEngagement = {
+            date: putReq.body.date,
+            name: putReq.body.name,
+            address: putReq.body.address,
+            UserId: putReq.body.UserId,
+            PetId: putReq.body.PetId
+        };
+        
+        db.Engagement.update(updatedEngagement, {
+            where: { id: putReq.params.engagementId }
+        }).then((response) => {
+            putRes.status(200).send(response); 
+        }).catch((err) => {
+            putRes.status(500).send('Ooopsie whoopsie: ' + err);
+        });    
+    });    
 };

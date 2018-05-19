11097a61de4c29d99aeb857ad3eee57f6a7c7ce3
n8morton <n8morton@gmail.com>
2018-05-19 11:19:36 -0400

    pushing for wynn

--- assets/scripts/pages/profilePage.js
+++ assets/scripts/pages/profilePage.js
@@ -3,5 +3,14 @@ $(document).ready(function () {
 
   $('#calendar').fullCalendar({
     // put your calendar options and callbacks here
-  })
+  });
+
+  $("#createEngagementButton").on("click", function(){
+    console.log("create engaement button clicked");
+
+    $("#people").click(function () {
+      $("#people-modal").addClass("is-active");
+    });
+
+  });
 });

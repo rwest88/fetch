4a5d0bfc45634b87dcdb848fb6594c69e951b9b3
Wynn Blevins <wynnblevins@yahoo.com>
2018-05-20 19:57:00 -0400

    Committing for riggity-ryan

--- assets/scripts/pages/profilePage.js
+++ assets/scripts/pages/profilePage.js
@@ -1,14 +1,19 @@
 $(document).ready(function () {
   
+  function createPet() {
+    console.log('inside create pet method...');
+    console.log('write post request code to /api/pet here...');
+    console.log('have this method return the promise from the jquery POST');
+  }
 
   $('#calendar').fullCalendar({
     // put your calendar options and callbacks here
   });
 
   $("#createEngagementButton").on("click", function(){
-    console.log("create engagement button clicked");
     $("#addEngagementModal").addClass("is-active");
   });
+  
   $("#close-button").on("click", function(){
     //console.log("create engagement button clicked");
     $("#addEngagementModal").removeClass("is-active");

c987d5ab297e07af1fd86d99f5896d8da899997d
Wynn Blevins <wynnblevins@yahoo.com>
2018-05-21 18:31:05 -0400

    Engagements are being created in the database successfully!

--- assets/scripts/services/dataServices/engagementService.js
+++ assets/scripts/services/dataServices/engagementService.js
@@ -10,7 +10,7 @@ var engagementService = (function ($) {
         return $.ajax({
           method: 'POST',
           url: '/api/engagement',
-          body: data
+          data: data
         });
       }
     };
